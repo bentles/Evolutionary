faf5c0dd6c58dcd8cf2803fa9e056fee660f2e02
bentles <dee.bent.lee@gmail.com>
2015-09-10 11:25:38 +0200

    A really ugly GA (that works)

--- Genetic Algorithm/src/ga.cpp
+++ Genetic Algorithm/src/ga.cpp
@@ -1,10 +1,11 @@
 #include "ga.h"
-#include <float.h>
 
 namespace ga
 {
-    double GeneticAlgorithm::getFitness(int i)
-    {
+    std::random_device GeneticAlgorithm::rd;
+    std::mt19937 GeneticAlgorithm::mt(GeneticAlgorithm::rd());
+    
+    double GeneticAlgorithm::getFitness(int i) {
         double income =
             chromosome[i][0] * 3000 +
             chromosome[i][1] * 3100 +
@@ -33,7 +34,7 @@ namespace ga
             chromosome[i][2] * 35 +
             chromosome[i][3] * 20;
 
-        double elec_expense = exp(0.01f*elec_kwh);
+        double elec_expense = exp(0.01*elec_kwh);
 
         double copper_expense = 0;
         if (copper_kg <= 8)
@@ -53,76 +54,179 @@ namespace ga
         return income - total_expense;            
     }
 
-    void GeneticAlgorithm::init()
-    {
-        for (int i = 0; i < chromosome.size(); i++) {
-            for (int j = 0; j < chromosome[0].size(); j++) {                
-                int kg= (int)floor(dist(mt) * (51));
-                chromosome[i][j] = kg;
-            }
-        }
-    }
-
-    vector<int> GeneticAlgorithm::selectForTournament()
-    {
+    vector<int> GeneticAlgorithm::selectForTournament() {
         vector<int> selection;
+        std::uniform_real_distribution<double> dist(0.0,1.0);
         int select_size = (int)floor(chromosome.size() * tournament_percent);
 
         for (int i = 0; i < select_size; i++) {
-            selection.push_back((int)floor(dist(mt) * (51)));            
-        }        
+            selection.push_back((int)floor(dist(mt) * (pop_size)));            
+        }
+
+        return selection;
     }
 
-    int GeneticAlgorithm::selectTournament()
-    {
+    int GeneticAlgorithm::selectTournament() {
         vector<int> tourn = selectForTournament();
         int maxfitpos = 0;
         double maxfit = -DBL_MAX;
         for (int i = 0; i < tourn.size(); i++) {
             double fitness = getFitness(tourn[i]);
+            // std::cout << tourn[i] << ": " << fitness << std::endl ;
             if (fitness > maxfit)
             {
+                //std::cout << "BIGGER" << std::endl;
                 maxfit = fitness;
-                maxfitpos = i;
+                maxfitpos = tourn[i];
             }
         }
+        //std::cout << "PARENT:" << maxfitpos << std::endl;
         return maxfitpos;
     }
 
     void GeneticAlgorithm::crossOverMutate(int p1, int p2,
                                            vector<double>& child1,
-                                           vector<double>& child2)
-    {
-        for (int i = 0; i < chromosome[0].size(); i++)
-        {
-            double crossover = (dist(mt));
-            double mutate1 = (dist(mt));
-            double mutate2 = (dist(mt));
-            double mutation1 = mutate1 > mutate_chance? ndist(mt) : 0;
-            double mutation2 = mutate2 > mutate_chance? ndist(mt) : 0;            
-            if (crossover > 0.5)
-            {
-                child1.push_back(chromosome[p1][i] + mutation1);
-                child2.push_back(chromosome[p2][i] + mutation2);
+                                           vector<double>& child2) {
+        std::uniform_real_distribution<double> dist(0.0,1.0);
+        std::normal_distribution<double> ndist(0.0,stddev);
+        for (int i = 0; i < chromosome[0].size(); i++)  {
+            double crossover = dist(mt);
+            double mutate1 = dist(mt);
+            double mutate2 = dist(mt);
+            double mutation1 = mutate1 < mutate_chance? ndist(mt) : 0;
+            double mutation2 = mutate2 < mutate_chance? ndist(mt) : 0;
+            double mutated1 = chromosome[p1][i] + mutation1 < 0 ? 0 :
+                chromosome[p1][i] + mutation1;
+            double mutated2 = chromosome[p2][i] + mutation2 < 0 ? 0 :
+                chromosome[p2][i] + mutation2;   
+            
+            if (crossover > 0.5) {              
+                child1.push_back(mutated1);
+                child2.push_back(mutated2);
             }
-            else
-            {
-                child1.push_back(chromosome[p2][i] + mutation1);
-                child2.push_back(chromosome[p1][i] + mutation2);
+            else {
+                child1.push_back(mutated2);
+                child2.push_back(mutated1);
             }
+        }                
+    }
+
+    void GeneticAlgorithm::runFor(int iterations) {
+        for (int i = 0; i < iterations; i++) {
+            //printPopulation();
+            vector<vector<double> > children;
+            while (children.size() < chromosome.size()) {            
+                vector<double> child1;
+                vector<double> child2;
+                int parent1 = selectTournament();
+                int parent2 = selectTournament();
+                //std::cout <<"----------------------PARENTS CHOSEN---------------------:" << std::endl;
+                //std::cout << parent1 << ":" << parent2 << std::endl;
+                crossOverMutate(parent1, parent2, child1, child2);
+                children.push_back(child1);
+                children.push_back(child2);
+            }
+            int pos = getMaxFitPos();
+            double fit = getFitness(pos); 
+            if (fit > best_fitness)
+                newBest(pos, fit);
+
+            if (verbose && (i % 1000) == 0 ) {
+                std::cout << "Iteration: " << i << std::endl;
+                std::cout << "Fitness: " << fit << std::endl;
+                std::cout << "Alloys: " ;
+                std::cout << "\t1: " << chromosome[pos][0] << ",";
+                std::cout << "\t2: " << chromosome[pos][1] << ",";
+                std::cout << "\t3: " << chromosome[pos][2] << ",";
+                std::cout << "\t4: " << chromosome[pos][3] << std::endl; 
+            }
+            //gg old gen
+            chromosome = children;
         }
 
-                
+        std::cout << "Fitness: " << best_fitness << std::endl;
+        std::cout << "Alloys: " ;
+        std::cout << "\t1: " << best_chromo[0] << ",";
+        std::cout << "\t2: " << best_chromo[1] << ",";
+        std::cout << "\t3: " << best_chromo[2] << ",";
+        std::cout << "\t4: " << best_chromo[3] << std::endl; 
     }
 
-    void GeneticAlgorithm::runFor(int i)
+    int GeneticAlgorithm::getMaxFitPos() {
+        double maxfit = -DBL_MAX;
+        int fitpos = 0;
+        for (int i = 0; i < chromosome.size(); i++) {
+            double fitness = getFitness(i);
+            if (fitness > maxfit)
+            {
+                maxfit = fitness;
+                fitpos = i;
+            }
+        }
+        return fitpos;
+    }
+
+    GeneticAlgorithm::GeneticAlgorithm(
+        int pop_size,
+        double stddev,
+        double tourn_percent,
+        double mutate_chance) :
+        pop_size(pop_size),
+        stddev(stddev),
+        verbose(false),
+        tournament_percent(tourn_percent),
+        mutate_chance(mutate_chance), best_fitness(-DBL_MAX) {
+        std::uniform_real_distribution<double> dist(0.0,1.0);
+        for (int i = 0; i < pop_size; i++) {
+            vector<double> cur;
+            for (int j = 0; j < 4; j++) {                
+                int kg= (int)floor(dist(mt) * (51));
+                cur.push_back(kg);
+            }
+            chromosome.push_back(cur);
+        }
+    }
+
+    GeneticAlgorithm GeneticAlgorithm::Verbose(bool isverbose) {
+        verbose = isverbose;
+        return *this;
+    }
+
+    void GeneticAlgorithm::newBest(int i, double fitness)
     {
+        best_fitness = fitness;
+        best_chromo = chromosome[i];
+
+        std::cout << "NEW RECORD!" << std::endl;
+        std::cout << "Fitness: " << std::setprecision(15) << best_fitness << std::endl;
+        std::cout << "Alloys: " << std::setprecision(5) ;
+        std::cout << "\t1: " << best_chromo[0] << ",";
+        std::cout << "\t2: " << best_chromo[1] << ",";
+        std::cout << "\t3: " << best_chromo[2] << ",";
+        std::cout << "\t4: " << best_chromo[3] << std::endl; 
         
-
     }
-
-    GeneticAlgorithm::GeneticAlgorithm()
+    void GeneticAlgorithm::printPopulation()
     {
-
+        for (int i = 0; i < chromosome.size(); i++) {
+            std::cout << i << ":\t";
+            for (int j = 0; j < chromosome[i].size(); j++) {
+                std::cout << chromosome[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 }
+
+    int main() {
+        using namespace ga;
+        
+        GeneticAlgorithm ga(
+            200, //pop_size
+            0.3,//std_dev
+            0.1, //percent of population chosen per tournament
+            0.2); //mutation chance
+        ga.Verbose(false);
+        ga.runFor(10000000);        
+        return 0;
+       }
--- Genetic Algorithm/src/ga.h
+++ Genetic Algorithm/src/ga.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <math.h>
 #include <random>
+#include <float.h>
+#include <iostream>
+#include <iomanip>
 
 using std::vector;
 
@@ -12,25 +15,34 @@ namespace ga
     class GeneticAlgorithm
     {
     private:
-        std::random_device rd;
-        std::mt19937 mt; //(rd());
-        std::uniform_real_distribution<double> dist;//(start, end)
-        std::normal_real_distribtion<double> ndist;//(mean, stddev)
+        static std::random_device rd;
+        static std::mt19937 mt; //(rd());
+        vector<double> best_chromo;
+        double best_fitness;
+        int pop_size;
+        double stddev;
         bool verbose;
         double tournament_percent; //percent of population in each tournament
-        int num_tournaments;
         double mutate_chance;
         vector<vector<double> > chromosome;
         double getFitness(int i);
-        void init();
         void crossOverMutate(int parent1, int parent2,
                                        vector<double>& child1, vector<double>& child2);
         vector<int> selectForTournament();
         int selectTournament();
         void crossOverMutate();
+        void printPopulation();
+        int getMaxFitPos();
+        void newBest(int i, double fitness);
     public:
         void runFor(int i);
-        GeneticAlgorithm();
+        GeneticAlgorithm Verbose(bool isverbose);
+
+        GeneticAlgorithm(
+            int pop_size,
+            double stddev,
+            double tourn_percent,
+            double mutate_chance);
 
     };
 }
